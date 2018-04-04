68322bd5d3c9fb6bac89c8f6d8f11cf7c9b0f948
kumasys <kumagai@kumasys.com>
2018-04-04 10:46:20 +0900

    added admin page setting form

--- dropbox_salesforce.php
+++ dropbox_salesforce.php
@@ -11,71 +11,103 @@ Description: send attachment to dropbox and form data to salesforce
 Version: 0.1
 */
 
-function hello_dolly_get_lyric() {
-	/** These are the lyrics to Hello Dolly */
-	$lyrics = "Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-We feel the room swayin'
-While the band's playin'
-One of your old favourite songs from way back when
-So, take her wrap, fellas
-Find her an empty lap, fellas
-Dolly'll never go away again
-Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-We feel the room swayin'
-While the band's playin'
-One of your old favourite songs from way back when
-Golly, gee, fellas
-Find her a vacant knee, fellas
-Dolly'll never go away
-Dolly'll never go away
-Dolly'll never go away again";
+include 'include/application_form.php';
+include 'include/setting_form.php';
 
-	// Here we split it into lines
-	$lyrics = explode( "\n", $lyrics );
+// add short code [example] that can be used to embed in post 
+function just_for_start(){
+	$information = "this is custom information";
+	// if you use echo instead of return message will print out in the begging of the post message
+	return $information;
+}
+// add_shortcode('name of short coed', 'function to call')
+add_shortcode('example', 'just_for_start');
 
-	// And then randomly choose a line
-	return wptexturize( $lyrics[ mt_rand( 0, count( $lyrics ) - 1 ) ] );
+
+function dbsf_example(){
+	return true;
 }
 
-// This just echoes the chosen line, we'll position it later
-function hello_dolly() {
-	$chosen = hello_dolly_get_lyric();
-	echo "<p id='dolly'>$chosen</p>";
+function dbsf_admin_menu_option(){
+	add_menu_page('Dropbox Salesforce Form', '応募者フォーム', 'manage_options', 'dbsf-admin-menu', 'dbsf_setting_page','',200);
 }
 
-// Now we set that function up to execute when the admin_notices action is called
-add_action( 'admin_notices', 'hello_dolly' );
+add_action('admin_menu', 'dbsf_admin_menu_option');
 
-// We need some CSS to position the paragraph
-function dolly_css() {
-	// This makes sure that the positioning is also good for right-to-left languages
-	$x = is_rtl() ? 'left' : 'right';
 
-	echo "
-	<style type='text/css'>
-	#dolly {
-		float: $x;
-		padding-$x: 15px;
-		padding-top: 5px;		
-		margin: 0;
-		font-size: 11px;
+function get_dbsf_options(){
+	$option = get_option('dbsf_options');
+	return $option;
+}
+
+function update_dbsf_options($options){
+	update_option('dbsf_options', $options);
+}
+
+function default_dbsf_options(){
+	$dbsf_options = array("db_id" => "",
+					  "db_secret" => "",
+					  "db_url" => "",
+					  "sfdc_id" => "",
+					  "sfdc_secret" => "",
+					  "sfdc_user" => "",
+					  "sfdc_pw" => "",
+					  "sfdc_url" => "");
+	return $dbsf_options;
+}
+
+
+function default_dbsf_form(){
+	$dbsf_form = "";
+	return $dbsf_form;
+}
+
+
+function save_dbsf_options_from_post($post){
+	
+	$dbsf_options = default_dbsf_options();
+
+	if(array_key_exists('_dbsfconf', $post)){
+		foreach ($_POST as $key => $value) {
+			$dbsf_options[$key] = $value;
+		}
+		try{
+			update_option('dbsf_options', $dbsf_options);
+			update_option('dbsf_form', $_POST['dbsf_form']);
+		} catch (Exception $e) {
+			var_dump($e->getMessage());
+		}
 	}
-	</style>
-	";
 }
 
-add_action( 'admin_head', 'dolly_css' );
+
+function dbsf_setting_page(){
+	
+	if($_POST){
+		save_dbsf_options_from_post($_POST);
+	}
+
+	try{
+		$dbsf_options = get_option('dbsf_options');
+		$dbsf_form = get_option('dbsf_form');
+	} catch (Exception $e){
+		var_dump($e->getMessage());
+		$dbsf_options = default_dbsf_options();
+		$dbsf_form = default_dbsf_form();
+	}
+	dbfs_admin_setting_form($dbsf_options, $dbsf_form);
+}
+
+
+// application form tag
+function create_dbsf_form(){
+	
+	if(isset($_POST)){
+		var_dump($_POST);
+	}
+
+	dbfs_application_form();
+}
+add_shortcode('dbsf_application_form', 'create_dbsf_form');
 
 ?>
